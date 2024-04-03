b2ae7f017118b08763241b92becef8fab1853b44
m1ss1k <74295921+M1ss1k@users.noreply.github.com>
2024-04-03 11:31:11 +0300

    adding adin legacy cog ( using old commands ) updating main.py for syncing slash commands

--- cogs/admin-legacy.py
+++ cogs/admin-legacy.py
@@ -7,49 +7,54 @@ connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
 
 
-class Administartion(commands.Cog):
+class AdministartionLegacy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     # ----- BAN USER -----
-    @app_commands.command(name='ban', description='Забанить пользователя')
-    async def ban_user(self, interaction: discord.Interaction, member: discord.Member , reason: str):
+    @commands.command(name='ban', description='Забанить пользователя')
+    async def ban_user(self, ctx, member: discord.Member , reason: str):
         ban_embed = discord.Embed(title='БАН')
         ban_embed.add_field(name='', value=f'Пользователь {member.name} был забанен на этом сервер по причине {reason}')
         await member.ban(reason=reason)
-        return await interaction.response.send_message(embed=ban_embed)
+        return await ctx.send(embed=ban_embed)
     # ----- UNBAN USER -----
-    @app_commands.command(name='unban', description='Разбанить игрока')
-    async def unban_user(self, interaction: discord.Interaction, member: discord.Member):
+    @commands.command(name='unban', description='Разбанить игрока')
+    async def unban_user(self, ctx, member: discord.Member):
         unban_embed = discord.Embed(title='РАЗБАН ИГРОКА')
         unban_embed.add_field(name='', value=f'Пользователь {member.name} был разбанен.')
         await member.unban()
-        return await interaction.response.send_message(embed=unban_embed)
+        return await ctx.send(embed=unban_embed)
     
     # ----- KICK USER -----
-    @app_commands.command(name='kick', description='Выгнать участника')
-    async def kick_player(self, interaction: discord.Interaction, member: discord.Member, reason: str):
+    @commands.command(name='kick', description='Выгнать участника')
+    async def kick_player(self, ctx, member: discord.Member, reason: str):
         kick_embed = discord.Embed(title='ВЫГОНЕНИЕ')
         kick_embed.add_field(name='', value=f'Пользователя {member.name} выгнали.')
         await member.kick(reason=reason)
-        return await interaction.response.send_message(embed=kick_embed)
+        return await ctx.send(embed=kick_embed)
     
     # ----- WARN USER -----
-    @app_commands.command(name='warn', description='Выдать предупреждение')
-    async def warn_player(self, interaction: discord.Interaction, member: discord.Member, reason: str):
+    @commands.command(name='warn', description='Выдать предупреждение')
+    async def warn_player(self, ctx, member: discord.Member, reason: str):
         warn_embed = discord.Embed(title='ПРЕДУПРЕЖДЕНИЕ')
         warn_embed.add_field(name='', value=f'Пользователь {member.name} получает предупреждение по причине: {reason}.')
         cursor.execute(f"INSERT INTO warnings (id, reason) VALUES {(member.id, reason)}")
-        return await interaction.response.send_message(embed=warn_embed)
+        connection.commit()
+        return await ctx.send(embed=warn_embed)
     
-    
-            
-
+ 
+    @commands.command(name='warnings')
+    async def player_warnings(self, ctx, member: discord.Member):
+        warnings_embed = discord.Embed(title='ПРЕДУПРЕЖДЕНИЯ УЧАСТНИКА')
+        warnings_embed.add_field(name='', value=f'Участник {member.mention} получил следующие предупреждения:')
+        cursor.execute(f"SELECT FROM warnings WHERE id={member.id}")
+        cursor.fetchone()
         
 
     
 
 async def setup(client):
-    await client.add_cog(Administartion(client))
+    await client.add_cog(AdministartionLegacy(client))
 
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from discord import app_commands, Intents, Embed
+from discord import app_commands, Intents, Embed, Interaction
+import discord
+from typing import Literal, Optional
+
 from discord.ext import commands
 import os, configparser
 
@@ -16,7 +19,7 @@ async def setup_hook():
         if filename.endswith('.py'):
 
             await bot.load_extension(f'cogs.{filename[:-3]}')
-            print(f"Loaded Cog: {filename[:-3]}")
+            print(f"Loaded Cog: {filename[:-3]}")  
         else:
             print("Unable to load pycache folder.")
 
@@ -27,6 +30,40 @@ async def on_member_join(member):
     join_embed.add_field(name='',value=f'Новый участник {member.mention} прыгнул на сервер!')
     join_channel = bot.get_channel(1219942526925148180)
     await join_channel.send(embed=join_embed)
+
+
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
 
 bot.run(general['token'])
